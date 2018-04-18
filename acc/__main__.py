23701ce74ddd8476f6da4ef5efd17db3dac7d7b7
katRover1234 <katRover1234@gmail.com>
2018-04-18 08:12:43 -0400

    Make more improvements

--- acc/__main__.py
+++ acc/__main__.py
@@ -58,10 +58,10 @@ def main():
 
     #print(str(os.getpid()) + " <> " + str(listen_pid))
 
-    # if os.getpid() != listen_pid:
-    #acc_instance = acc.ACC(command_queue, 150, 80)
-    # acc_instance.run()
-    # acc.main(command_queue)
+    #if os.getpid() != listen_pid:
+    acc_instance = acc.ACC(command_queue, 150, 30)
+    acc_instance.run()
+    #acc.main(command_queue)
 
 
 if __name__ == "__main__":
